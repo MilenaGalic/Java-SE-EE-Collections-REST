f7db07c4b3075ad7583e859e69a53fb41ab5a01e
milanlatinovic <milan.softline@gmail.com>
2016-07-04 00:11:04 +0200

    New repo organization
    Added sections
    Basics VS REST

---
+++ 2 - REST Web Services using Java EE/RESTClientTemplate/src/main/RESTMain.java
@@ -0,0 +1,26 @@
+package main;
+
+/**
+ * Calls the RESTful web service at REST_URL defined below
+ * 
+ * @author milan_000
+ *
+ */
+public class RESTMain {
+	
+	public static final String REST_URL = "www.url.com";
+
+	/**
+	 * Call the web service and display the response
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// call the service and get the response object
+		
+		// process the response object
+
+	}
+
+}
